d53fb53ebcd7df59670529289c96fca61475ac44
kamyshks <lexica2000@mail.ru>
2022-09-08 11:24:38 +0300

    fix for

--- src/main/java/kamyshks/services/EndpointService.java
+++ src/main/java/kamyshks/services/EndpointService.java
@@ -35,25 +35,21 @@ public class EndpointService {
     }
 
     private void callGetAmount(final List<Integer> idList){
-        for (int i=0; i < 10; i++) {
-            final Integer id = idList.get(getRandomIndex(idList.size()));
-            final String url = baseUrl + "/api/amount/" + id;
-            Response response = ClientBuilder.newBuilder().build().target(url)
-                    .request(MediaType.APPLICATION_JSON)
-                    .get();
-            System.out.println("GET " + url + " " + response.getStatusInfo());
-        }
+        final Integer id = idList.get(getRandomIndex(idList.size()));
+        final String url = baseUrl + "/api/amount/" + id;
+        Response response = ClientBuilder.newBuilder().build().target(url)
+                .request(MediaType.APPLICATION_JSON)
+                .get();
+        System.out.println("GET " + url + " " + response.getStatusInfo());
     }
 
     private void callAddAmount(final List<Integer> idList){
-        for (int i=0; i < 10; i++) {
-            final Integer id = idList.get(getRandomIndex(idList.size()));
-            String url = baseUrl + "/api/amount/";
-            Response response = ClientBuilder.newBuilder().build().target(url)
-                    .request(MediaType.APPLICATION_JSON)
-                    .post(Entity.entity(new BalanceDto(id, 1L), MediaType.APPLICATION_JSON));
-            System.out.println("POST " + url + " " + response.getStatusInfo());
-        }
+        final Integer id = idList.get(getRandomIndex(idList.size()));
+        String url = baseUrl + "/api/amount/";
+        Response response = ClientBuilder.newBuilder().build().target(url)
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.entity(new BalanceDto(id, 1L), MediaType.APPLICATION_JSON));
+        System.out.println("POST " + url + " " + response.getStatusInfo());
     }
 
     private int getRandomIndex(final int idListSize){
